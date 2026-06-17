4cd40854707e1fb50dc10c04bb1d76d6a2276a7a
maheswar189 <maheswarreddy189@gmai.com>
2026-06-17 08:59:48 +0530

    ReactJS Tutorial 49: Making API Call In React

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import { useEffect } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "0786e8e4e5a345c193dc2313d41b8607";
+
 export default function Search() {
   const [query, setQuery] = useState("pizza");
+
   //Syntax
   useEffect(() => {
-    function demo() {
-      console.log("demo function called");
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
     }
-    demo();
+    fetchFood();
   }, [query]);
 
   return (
