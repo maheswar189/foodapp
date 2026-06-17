0f751cbd41511b6913876a56a616f6d635f67a48
maheswar189 <maheswarreddy189@gmai.com>
2026-06-17 09:10:47 +0530

    ReactJS Tutorial 50: Creating State To Save Food Data

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "0786e8e4e5a345c193dc2313d41b8607";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
 
   //Syntax
@@ -13,6 +13,7 @@ export default function Search() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
