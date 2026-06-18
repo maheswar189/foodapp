572d92f137ed05a139d98255fd609da7c2ac7400
maheswar189 <maheswarreddy189@gmai.com>
2026-06-18 06:58:54 +0530

    ReactJS Tutorial 53: Creating The Nav Component

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import Search from "./components/Search.jsx";
 import FoodList from "./components/FoodList.jsx";
+import Nav from "./components/Nav.jsx";
+import "./App.css";
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <FoodList foodData={foodData} />
     </div>
---
+++ foodapp/src/components/Nav.jsx
@@ -0,0 +1,4 @@
+import style from "./Nav.module.css";
+export default function Nav() {
+  return <div className={style.nav}>🍔 Food APP </div>;
+}
