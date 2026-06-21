cf63ec3d062c9fce399edc284836b7cf35283ca7
maheswar189 <maheswarreddy189@gmai.com>
2026-06-21 07:02:49 +0530

    ReactJS Tutorial 56: Outer Container Component

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -3,13 +3,16 @@ import Search from "./components/Search.jsx";
 import FoodList from "./components/FoodList.jsx";
 import Nav from "./components/Nav.jsx";
 import "./App.css";
+import Container from "./components/Container.jsx";
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <FoodList foodData={foodData} />
+      </Container>
     </div>
   );
 }
