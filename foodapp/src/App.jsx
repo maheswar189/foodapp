9cd9d615a72e1a8542180b7726b5ecccf8afdf9a
maheswar189 <maheswarreddy189@gmai.com>
2026-06-22 07:55:41 +0530

    ReactJS Tutorial 58: Creating The Food Detail Component

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -5,15 +5,20 @@ import Nav from "./components/Nav.jsx";
 import "./App.css";
 import Container from "./components/Container.jsx";
 import InnerContainer from "./components/InnerContainer.jsx";
+import FoodDetails from "./components/FoodDetails.jsx";
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("");
   return (
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Container>
         <InnerContainer>
-          <FoodList foodData={foodData} />
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId}></FoodDetails>
         </InnerContainer>
       </Container>
     </div>
--- foodapp/src/components/FoodList.jsx
+++ foodapp/src/components/FoodList.jsx
@@ -1,10 +1,14 @@
 import FoodItem from "./FoodItem.jsx";
-export default function FoodList({ foodData }) {
+export default function FoodList({ foodData, setFoodId }) {
   return (
     <div>
       {Array.isArray(foodData)
         ? foodData.map((food) => (
-            <FoodItem key={food.id || food.title} food={food} />
+            <FoodItem
+              setFoodId={setFoodId}
+              key={food.id || food.title}
+              food={food}
+            />
           ))
         : null}
     </div>
